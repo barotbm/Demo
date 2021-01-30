c961893bb6be5d789a242c586f89dfe826456991
Bhavik Barot <bhavikb@aeonsoftware.com.au>
2021-01-30 12:37:36 -0500

    test

--- Demo.Core/Entities/Category.cs
+++ Demo.Core/Entities/Category.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Demo.Core.Entities
 {
-    // test pr integration
+    // test pr integration 
     public class Category : Entity
     {
         public Category()
